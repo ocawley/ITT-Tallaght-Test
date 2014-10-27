37eca24dd1ddca34fedaf52e9bfd67b7a4fe4020
ocawley <oisin.cawley@ittdublin.ie>
2014-10-27 10:51:44 +0000

    New version of grades

--- oisin.sh
+++ oisin.sh
@@ -1,5 +1,21 @@
 # Test case 1
-java grades 50 90
+# Inputs: 50 90
+# Expected output: Pass
+result="$java grades 50 90"
+if [[ $result != "Pass" ]]
+then
+	echo Test Case 1 Failed
+else
+	echo Test Case 1 Passed
+fi
 
 # Test case 2
-java grades 100 100
+# Inputs: 150 90
+# Expected output: Invalid Input
+result="$java grades 150 50"
+if [[ $result != "Invalid Input" ]]
+then
+	echo Test Case 2 Failed
+else
+	echo Test Case 2 Passed
+fi
