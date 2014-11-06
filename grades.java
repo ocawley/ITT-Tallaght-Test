751272416a21c27a9c1a6cdf2eb8b710e71c0fe0
ocawley <oisin.cawley@ittdublin.ie>
2014-11-06 10:17:53 +0000

    Update grades.java

--- grades.java
+++ grades.java
@@ -1,27 +1,24 @@
 /**
  * @author ocawley
- * Updated ....
- * Second update
- * Third update
  */
 
 public class grades
 {
 	public static void main(String[] args) {
-1		String grade = new String();
-1		int exam, ca;
+		String grade = new String();
+		int exam, ca;
 
-1		exam = Integer.parseInt(args[0]);
-1		ca = Integer.parseInt(args[1]);
+		exam = Integer.parseInt(args[0]);
+		ca = Integer.parseInt(args[1]);
 
-5		if (exam < 0 || exam > 100 || ca < 0 || ca > 100) {
-6			grade = "Invalid input";
-7		} else if (exam < 40 || ca < 40) {
-8			grade = "Component Fail";
-9		} else {
-10			int combined = (60 * exam + 40 * ca) / 100;
-11			if (combined < 60) {
-12				grade = "Fail";
+		if (exam < 0 || exam > 100 || ca < 0 || ca > 100) {
+			grade = "Invalid input";
+		} else if (exam < 40 || ca < 40) {
+			grade = "Component Fail";
+		} else {
+			int combined = (60 * exam + 40 * ca) / 100;
+			if (combined < 60) {
+				grade = "Fail";
 			} else if (combined >= 60 && combined <= 80) {
 				grade = "Pass";
 			} else if (combined > 80 && combined <= 100) {
